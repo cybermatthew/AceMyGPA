57fbe9bb496051d2c08dbb1d3486df5b1804d321
Matthew Pick <mpick@stanford.edu>
2013-02-16 09:23:22 +0000

    Finished writing points form

--- gradecalc/pointcalc.php
+++ gradecalc/pointcalc.php
@@ -1,6 +1,15 @@
 <?PHP include("header.php"); ?>
 <?PHP include("sidebar.php"); ?>
+<?PHP include("calcLabels.php"); ?>
       <div id=pointPageText>
-      	   
+      	   <form name="input">
+	   	 Total Possible: <input type="text" name="totalPossible"><br>
+	  	  Your Points: <input type="text" name="yourPoints"><br> 
+		  <hr>
+	   	  Final Exam Points: <input type="text" name="finalPoints"><br>
+	   	  Desired Percent in Class: <input type="text" name="desiredPercent"><br>     	   
+	   	  <hr>
+	   	  <input type="submit" value="Calculate!">
+	   </form>
       </div>
 <?PHP include("footer.php"); ?>
