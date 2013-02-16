2ce2a88b4ea6abdf02ca7d66ca3eaabc5d14093c
Matthew Pick <mpick@stanford.edu>
2013-02-16 07:37:25 +0000

    Added bullets to home page.

--- gradecalc/index.php
+++ gradecalc/index.php
@@ -1,8 +1,19 @@
 <html>
-	<head>
-	<title>GradeCalc</title>
-	</head>
-	<body>
-		<h1>Hello World!</h1>
-	</body>
+  <head>
+    <title>GradeCalc</title>
+  </head>
+  <body>
+    <h1>GradeCalc</h1>
+    <div id=frontPageText>
+      <ul>
+	<li>Easily calculate the grades you need to ace your classes</li>
+	<li>Click here to see how!
+	  <br>
+	  - or -
+	  <br>
+	  Select a tab
+	</li>
+      </ul>
+    </div>
+  </body>
 </html>
