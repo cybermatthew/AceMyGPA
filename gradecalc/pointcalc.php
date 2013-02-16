6f0ada72184a1f4d39753ff5d3c19b66e046b91c
Matthew Pick <mpick@stanford.edu>
2013-02-16 10:20:52 +0000

    Server prints one error message

--- gradecalc/pointcalc.php
+++ gradecalc/pointcalc.php
@@ -2,7 +2,7 @@
 <?PHP include("sidebar.php"); ?>
 <?PHP include("calcLabels.php"); ?>
       <div id=pointPageText>
-      	   <form name="input">
+      	   <form action="pointcalc.php" method="post">
 	   	 Total Possible: <input type="text" name="totalPossible"><br>
 	  	  Your Points: <input type="text" name="yourPoints"><br> 
 		  <hr>
@@ -11,5 +11,15 @@
 	   	  <hr>
 	   	  <input type="submit" value="Calculate!">
 	   </form>
+	   <p>Necessary Grade on Final<br>
+	   	Points: <?php calculate(); ?>
+	  </p>
       </div>
 <?PHP include("footer.php"); ?>
+
+<?php
+function calculate(){
+	 if($_POST['totalPossible'] == ''){
+	 	echo 'Enter the total points possible.';
+	 }
+}
