320926344270ba71eed3a6f4a3c4eb54dd67aabf
Matthew Pick <mpick@stanford.edu>
2013-02-17 05:09:58 +0000

    Before changing error messages

--- gradecalc/header.php
+++ gradecalc/header.php
@@ -1,10 +1,10 @@
 <html>
   <head>
-    <title>GradeCalc</title>
+    <title>F mygpa</title>
   </head>
   <body>
     <div id=topPane>
       <a href="about.html">About</a>
       <a href="faq.html">FAQ</a>
     </div>
-    <div id=siteTitle><h1>GradeCalc</h1></div>
+    <div id=siteTitle><h1>F mygpa</h1></div>
