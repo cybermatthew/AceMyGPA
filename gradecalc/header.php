82f30ef5bb5b63641e24b7cc4bad4d3b63af1c56
Matthew Pick <mpick@stanford.edu>
2013-02-17 10:19:18 +0000

    Added binder background for homepage.

--- gradecalc/header.php
+++ gradecalc/header.php
@@ -1,10 +1,11 @@
 <html>
   <head>
     <title>F mygpa</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
   </head>
   <body>
     <div id=topPane>
       <a href="about.html">About</a>
       <a href="faq.html">FAQ</a>
+      <div id=siteTitle><h1>F mygpa</h1></div>
     </div>
-    <div id=siteTitle><h1>F mygpa</h1></div>
