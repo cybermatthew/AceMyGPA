738e89402a256baa958dd12bdd3dd2d5ad58c7f4
Matthew Pick <mpick@stanford.edu>
2013-02-19 02:17:36 +0000

    Buttons now centered properly

--- gradecalc/topbar.php
+++ gradecalc/topbar.php
@@ -1,6 +1,6 @@
-<div id=toptabs>
+<div id=topbar>
 	<div id=indexButton><a class="button" href="index.php">Home</a></div>
-     	 <div id=calculatorButton><a class="button" href="percentcalc.php">Calculator</a></div>
+     	 <div id=calculatorButton><a class="button" href="percentcalc.php">Calculator</a></div>  
     	  <div id=helpButton><a class="button" href="help.php">Help</a></div>
 </div>
 <br>
