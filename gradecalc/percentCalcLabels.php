6e2335b37dfd85cbf25a005ad539b9b73b8d2306
Matthew Pick <mpick@stanford.edu>
2013-02-19 20:28:30 +0000

    Put box around calculate by:

--- gradecalc/percentCalcLabels.php
+++ gradecalc/percentCalcLabels.php
@@ -1,5 +1,8 @@
 <div id=calcLabels>
+     <hr>
      <p>Calculate by:</p>
      <div id=percentLabel><a href="percentcalc.php">Percent &#x2713</a></div>
      <div id=pointLabel><a href="pointcalc.php">Points</a></div>
+     <br>
+     <hr>
 </div>
--- gradecalc/pointsCalcLabels.php
+++ gradecalc/pointsCalcLabels.php
@@ -1,5 +1,8 @@
 <div id=calcLabels>
+     <hr>
      <p>Calculate by:</p>
      <div id=percentLabel><a href="percentcalc.php">Percent</a></div>
      <div id=pointLabel><a href="pointcalc.php">Points &#x2713</a></div>
+     <br>
+     <hr>
 </div>
