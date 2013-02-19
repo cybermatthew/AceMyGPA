1d4f83df6905f78d205de180e6f485c6a33d0e7e
Matthew Pick <mpick@stanford.edu>
2013-02-19 00:19:01 +0000

    Added button hover.

--- gradecalc/topbar.php
+++ gradecalc/topbar.php
@@ -1,6 +1,6 @@
 <div id=toptabs>
 	<div id=indexButton><a class="button" href="index.php">Home</a></div>
-     	  <a class="button" href="percentcalc.php">Calculator</a>
-    	  <a class="button" href="help.php">Help</a>
+     	 <div id=calculatorButton><a class="button" href="percentcalc.php">Calculator</a></div>
+    	  <div id=helpButton><a class="button" href="help.php">Help</a></div>
 </div>
 <br>
