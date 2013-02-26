95e8350f6b6ce8e2ee023f41643ddbeefe6a39e0
Matthew Pick <mpick@stanford.edu>
2013-02-26 23:03:01 +0000

    Added all rows and columns to table.  Need to add math and printout.

---
+++ gradecalc/college.php
@@ -0,0 +1,67 @@
+<?PHP include("header.php"); ?>
+      <div id=collegeForm>
+      	   <table border="1">
+		<tr>
+		<div id=topLabels>
+			<td></td>
+			<td>1 Unit</td>
+			<td>2 Units</td>
+			<td>3 Units</td>
+			<td>4 Units</td>
+			<td>5 Units</td>
+		</div>
+		</tr>
+		<tr>
+		<div id=sideLabels>
+			<td>A | </td>
+		</div>
+			<td><input type="text" name="A1" size="10"></td>
+			<td><input type="text" name="A2" size="10"></td>
+			<td><input type="text" name="A3" size="10"></td>
+			<td><input type="text" name="A4" size="10"></td>
+			<td><input type="text" name="A5" size="10"></td>
+		</tr>
+		<tr>
+		<div id=sideLabels>
+			<td>B | </td>
+		</div>
+		      	<td><input type="text" name="B1" size="10"></td>
+			<td><input type="text" name="B2" size="10"></td>
+			<td><input type="text" name="B3" size="10"></td>
+			<td><input type="text" name="B4" size="10"></td>
+			<td><input type="text" name="B5" size="10"></td>
+		</tr>
+		<tr>  
+		<div id=sideLabels>  
+                        <td>C | </td>
+			
+		</div>
+			<td><input type="text" name="C1" size="10"></td>
+			<td><input type="text" name="C2" size="10"></td>
+			<td><input type="text" name="C3" size="10"></td>
+			<td><input type="text" name="C4" size="10"></td>
+			<td><input type="text" name="C5" size="10"></td>
+                </tr>
+		<tr>  
+		<div id=sideLabels>  
+                        <td>D | </td>
+		</div>
+			<td><input type="text" name="D1" size="10"></td>
+			<td><input type="text" name="D2" size="10"></td>
+			<td><input type="text" name="D3" size="10"></td>
+			<td><input type="text" name="D4" size="10"></td>
+			<td><input type="text" name="D5" size="10"></td>
+                </tr>
+		<tr>    
+		<div id=sideLabels>
+                        <td>F | </td>
+			<td><input type="text" name="F1" size="10"></td>
+			<td><input type="text" name="F2" size="10"></td>
+			<td><input type="text" name="F3" size="10"></td>
+			<td><input type="text" name="F4" size="10"></td>
+			<td><input type="text" name="F5" size="10"></td>
+		</div>
+                </tr>
+	   </table>
+     </div>
+<?PHP include("footer.php"); ?>
