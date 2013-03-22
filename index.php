5169e5721b54ce1c89bbfffdb76af39d7a438be5
Matthew Pick <mpick@stanford.edu>
2013-03-22 08:06:10 +0000

    Centered youtube video

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<br><li>GPA - calculate your high school or college GPAs
 	<li>Final Exam - easily calculate the grade you need on your final exam to ace your class</li>
       </ul>
-      <iframe width="400" height="225" src="http://www.youtube.com/embed/dgIvCN1GMY8?rel=0" frameb\
-order="0" allowfullscreen></iframe>
+      <center><iframe width="400" height="225" src="http://www.youtube.com/embed/dgIvCN1GMY8?rel=0" frameb\
+order="0" allowfullscreen></iframe></center>
     </div>
 <?PHP include("footer.php"); ?>
